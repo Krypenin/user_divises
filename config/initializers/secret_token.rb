74f7ea9cbda5035f492b24cdd233c58c618b3a70
Krypenin Kostya <krypenin@gmail.com>
2013-04-27 03:54:28 +0400

    git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opros::Application.config.secret_token = '7f585baeaf62592f088d210eb46ef982e2dea836e81c160a1d6c15c96375bd5d65e3e82ef17333d5c7fb97e86cb0c82d795f2c0c1f011c8d79310afc735affc5'
